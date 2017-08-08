218cfdcc72c3b76c88dcb04f191138e4af20acd1
Muhammad Owais Aslam <rana.owais.aslam@gmail.com>
2017-08-08 14:33:16 +0500

    Initial commit

---
+++ app/src/main/java/com/example/moandroidapps/simplelistview/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.moandroidapps.simplelistview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //####################################################################
+        //####################################################################
+        //1. Retrieve Data from the resource-file
+        String[] data = getResources().getStringArray(R.array.book_categories);
+        //####################################################################
+        //####################################################################
+        //2. Create Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                data);
+        //####################################################################
+        //####################################################################
+        //3. Get Reference to the ListView
+        ListView lv = (ListView) findViewById(R.id.simple_listview_for_book_categories);
+        //####################################################################
+        //####################################################################
+        //4. Bind Adapter to the ListView
+        lv.setAdapter(adapter);
+    }//end onCreate()
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
